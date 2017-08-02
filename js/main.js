66ffc7d01f9391435268fe770a83f272d2cd3958
Jose luis Yana <jose.yana@imedia.pe>
2017-08-02 16:09:28 -0500

    add word

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     function randomData() {
         return Math.round(Math.random()*1000);
     }
-    $.get('pe-all.geo.json', function (json) {
+    /*$.get('pe-all.geo.json', function (json) {
         echarts.registerMap('Peru', json);
         var chart = echarts.init(document.getElementById('container'));
         chart.setOption({
@@ -50,7 +50,8 @@ $(document).ready(function(){
          chart.on('click',function(param){
                 console.log(param);
             });
-    });
+    });*/
+
         /*$.get('peru_capital_provincia.json', function (json) {
             echarts.registerMap('Peru', json);
             var chart = echarts.init(document.getElementById('container2'));
@@ -80,5 +81,46 @@ $(document).ready(function(){
                     console.log(param);
                 });
     });*/
+        var chart = echarts.init(document.getElementById('container'));
+        var option = {
+            tooltip: {
+                trigger: 'item',
+                formatter: '{b}: {c}'
+            },
+            visualMap: {
+                min: 0,
+                max: 2500,
+                left: 'left',
+                top: 'bottom',
+                text: ['start','end'],           // 文本，默认为数值文本
+                calculable: true
+            },
+            series :{
+                type: 'map',
+                map: 'wordJson',
+                roam: true,
+                label: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: true
+                    }
+                },
+                data : [
+                    {
+                        name:'Bolivia',value:1000,selected:true
+                    }
+                ]
+            },
+            
+        }
+    $.get('world.json', function (json) {
+        echarts.registerMap('wordJson', json);        
+        chart.setOption(option);
+         chart.on('click',function(param){
+                console.log(param);
+            });
+    });
 });
 
